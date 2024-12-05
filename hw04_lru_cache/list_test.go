ccb66d0c124c787e8229a324e4c82ef75b1b680c
Elena Grishkova <lmetis@rambler.ru>
2024-12-06 00:41:51 +0300

    ДЗ-4 выполнена реализация двусвязного списка

--- hw04_lru_cache/list_test.go
+++ hw04_lru_cache/list_test.go
@@ -48,4 +48,70 @@ func TestList(t *testing.T) {
 		}
 		require.Equal(t, []int{70, 80, 60, 40, 10, 30, 50}, elems)
 	})
+
+	t.Run("customTest", func(t *testing.T) {
+		l := NewList()
+
+		// Добавление в пустой в начало
+		l.PushFront(10) // [10]
+		require.Equal(t, 1, l.Len())
+		frontItem := l.Front()
+		require.Equal(t, 10, frontItem.Value)
+		require.Nil(t, frontItem.Next)
+		require.Nil(t, frontItem.Prev)
+
+		// Делаем снова пустым
+		l.Remove(frontItem) // []
+		require.Equal(t, 0, l.Len())
+		require.Nil(t, frontItem.Next)
+		require.Nil(t, frontItem.Prev)
+
+		// Добавление в пустой в конец
+		l.PushBack(20) // [20]
+		require.Equal(t, 1, l.Len())
+		backItem := l.Back()
+		require.Equal(t, 20, backItem.Value)
+		require.Nil(t, backItem.Next)
+		require.Nil(t, backItem.Prev)
+
+		// Делаем снова пустым
+		l.Remove(backItem) // []
+		require.Equal(t, 0, l.Len())
+		require.Nil(t, backItem.Next)
+		require.Nil(t, backItem.Prev)
+
+		// Проверяем корректность связей
+		l.PushFront(10) // [10]
+		l.PushBack(20)  // [10, 20]
+		l.PushBack(30)  // [10, 20, 30]
+
+		frontItem = l.Front()
+		middleItem := l.Front().Next
+		backItem = l.Back()
+		checkValuesOfSize3(t, frontItem, middleItem, backItem, 10, 20, 30)
+		checkLinks(t, middleItem, frontItem, backItem)
+
+		// Проверяем корректность связей MoveToFront
+		l.MoveToFront(backItem)
+		frontItem = l.Front()
+		middleItem = l.Front().Next
+		backItem = l.Back()
+		checkValuesOfSize3(t, frontItem, middleItem, backItem, 30, 10, 20)
+		checkLinks(t, middleItem, frontItem, backItem)
+	})
+}
+
+func checkValuesOfSize3(t *testing.T, frontItem *ListItem, middleItem *ListItem, backItem *ListItem, expected1 interface{}, expected2 interface{}, expected3 interface{}) {
+	require.Equal(t, expected1, frontItem.Value)
+	require.Equal(t, expected2, middleItem.Value)
+	require.Equal(t, expected3, backItem.Value)
+}
+
+func checkLinks(t *testing.T, middleItem *ListItem, frontItem *ListItem, backItem *ListItem) {
+	require.Equal(t, middleItem, frontItem.Next)
+	require.Nil(t, frontItem.Prev)
+	require.Equal(t, backItem, middleItem.Next)
+	require.Equal(t, frontItem, middleItem.Prev)
+	require.Nil(t, backItem.Next)
+	require.Equal(t, middleItem, backItem.Prev)
 }
