cde6cf361fe1304c517e4ff7a1baec9fa61def01
Elena Grishkova <lmetis@rambler.ru>
2025-01-29 22:37:36 +0300

    ДЗ-7 выполнено основное задание

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,12 @@ package main
 
 import (
 	"errors"
+	"io"
+	"log"
+	"os"
+
+	//nolint:depguard
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -10,6 +16,59 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	fromFile, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer closeFile(fromFile)
+
+	fromInfo, err := fromFile.Stat()
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+
+	fileSize := fromInfo.Size()
+	if offset > fileSize {
+		return ErrOffsetExceedsFileSize
+	}
+	println("limit before = ", limit)
+	if limit == 0 || limit > fileSize {
+		limit = fileSize
+	}
+	limit = min(limit, fileSize-offset)
+	println("fileSize = ", fileSize)
+	println("offset = ", offset)
+	println("limit = ", limit)
+
+	toFile, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer closeFile(toFile)
+
+	// set the offset
+	_, err = fromFile.Seek(offset, io.SeekStart)
+	if err != nil {
+		return err
+	}
+
+	bar := pb.Full.Start64(limit)
+	// create proxy reader
+	barReader := bar.NewProxyReader(fromFile)
+	// copy from proxy reader
+	_, err = io.CopyN(toFile, barReader, limit)
+	if err != nil {
+		return err
+	}
+	// finish bar
+	bar.Finish()
+
 	return nil
+}
+
+func closeFile(fromFile *os.File) {
+	err := fromFile.Close()
+	if err != nil {
+		log.Panicf("failed to close source file: %v", err)
+	}
 }
