b3a1b1fd262fc66e86981f1dc96c56d741a751a5
Elena Grishkova <lmetis@rambler.ru>
2025-02-07 13:05:35 +0300

    ДЗ-11 выполнено основное задание

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
+	"errors"
+	"fmt"
 	"io"
+	"net"
 	"time"
 )
 
@@ -13,9 +17,62 @@ type TelnetClient interface {
 }
 
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &Telnet{
+		address:   address,
+		timeout:   timeout,
+		out:       out,
+		inScanner: bufio.NewScanner(in),
+	}
+}
+
+type Telnet struct {
+	address     string
+	timeout     time.Duration
+	conn        net.Conn
+	out         io.Writer
+	inScanner   *bufio.Scanner
+	connScanner *bufio.Scanner
+}
+
+func (t *Telnet) Connect() error {
+	conn, err := net.DialTimeout("tcp", t.address, t.timeout)
+	if err == nil {
+		t.conn = conn
+		t.connScanner = bufio.NewScanner(conn)
+	}
+	return err
+}
+
+func (t *Telnet) Close() error {
+	if t.conn != nil {
+		if err := t.conn.Close(); err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (t *Telnet) Send() error {
+	if !t.inScanner.Scan() {
+		return errors.New("no data to send")
+	}
+
+	line := t.inScanner.Bytes()
+	if _, err := t.conn.Write([]byte(fmt.Sprintf("%s\n", line))); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (t *Telnet) Receive() error {
+	if !t.connScanner.Scan() {
+		return errors.New("no data received")
+	}
+
+	line := t.connScanner.Bytes()
+	if _, err := t.out.Write([]byte(fmt.Sprintf("%s\n", line))); err != nil {
+		return err
+	}
+	return nil
+}
